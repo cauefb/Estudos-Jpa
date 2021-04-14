7a11bf07e62088b28e45be6394953017e4042366
cauefb <61958005+cauefb@users.noreply.github.com>
2021-04-14 12:22:09 -0300

    iniciando estudos

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll1(){
+		List<User> result = repository.findAll();
+		return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<User>> findAll(Pageable pageable) {
+	    Page<User> result = repository.findAll(pageable);
+	    return ResponseEntity.ok(result);
+	}
+	@GetMapping(value = "/search-salary")
+	public ResponseEntity<Page<User>> searchBySalary(@RequestParam(defaultValue = "0") Double minSalary, @RequestParam(defaultValue = "100000000000000") Double maxSalary,Pageable pegeable) {
+	    Page<User> result = repository.findBySalaryBetween(minSalary, maxSalary, pegeable);
+	    return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping(value = "/search-name")
+	public ResponseEntity<Page<User>> searchByName(
+			@RequestParam(defaultValue = "") String name,
+			Pageable pageable) {
+	    Page<User> result = repository.searchByName(name, pageable);
+	    return ResponseEntity.ok(result);
+	}
+}
